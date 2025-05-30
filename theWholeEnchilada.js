5bc631cbded3ee72f72f3422cf99657460adf491
Michael Iyer <your-email@example.com>
2025-05-30 01:23:26 -0400

    Quash in and out

--- theWholeEnchilada.js
+++ theWholeEnchilada.js
@@ -1396,11 +1396,11 @@ export const dailyWordsSmall = [
     'PAYEE', 'PAYER', 'PECAN', 'PENAL', 'PENCE', 'PENNY',
     'PEONY', 'PERIL', 'PESTO', 'PETAL', 'PETTY', 'PIGGY', 'PIVOT', 'PIZZA',
     'PLAID', 'PLAIN', 'PLANE', 'PLEAD', 'PLIED', 'PLIER', 'PLUME', 'PLUMP', 'PLUSH',
-    'POESY', 'POLAR', 'POOCH', 'POPPY', 'POSER', 'POSIT', 'POSSE', 'POUCH', 'POUTY',
+    'POESY', 'POOCH', 'POPPY', 'POSER', 'POSIT', 'POSSE', 'POUCH', 'POUTY',
     'PRANK', 'PRAWN', 'PREEN', 'PRIED', 'PRISM', 'PRIVY',  
     'PROOF', 'PRUDE', 'PUBIC', 'PUDGY', 'PUFFY', 'PULSE', 'PUNCH',
     'PUPPY', 'PUREE', 'PURER', 'PURSE', 'PUSHY', 'PUTTY', 'PYGMY', 'QUACK', 'QUAIL', 'QUAKE',
-    'QUARK', 'QUASH', 'QUASI', 'QUEER', 'QUELL', 'QUILL', 'QUILT', 'QUOTH',
+    'QUARK', 'QUASI', 'QUEER', 'QUELL', 'QUILL', 'QUILT', 'QUOTH',
     'RABBI', 'RABID', 'RACER', 'RADAR', 'RADII', 'RAJAH', 'RALLY', 'RALPH', 'RANDY',
     'RARER', 'RASPY', 'RATTY', 'RAVEN', 'RAZOR', 'REARM', 'REBAR', 
     'RECUT', 'REEDY', 'REFIT', 'REHAB', 'REIGN', 'RELAY', 'REMIT',
@@ -2291,7 +2291,7 @@ export const dailyWordsLarge = [
     'PLUTO', 'PLYER', 'POACH', 'POAKA', 'POAKE', 'POBOY', 'POCKS', 'POCKY', 'PODAL', 'PODDY',
     'PODEX', 'PODGE', 'PODGY', 'PODIA', 'POEMS', 'POEPS', 'POESY', 'POETS', 'POGEY', 'POGGE',
     'POGOS', 'POHED', 'POILU', 'POIND', 'POKAL', 'POKED', 'POKES',
-    'POKEY', 'POKIE', 'POLAR', 'POLED', 'POLER', 'POLES', 'POLEY', 'POLIO', 'POLIS', 'POLJE',
+    'POKEY', 'POKIE', 'POLED', 'POLER', 'POLES', 'POLEY', 'POLIO', 'POLIS', 'POLJE',
     'POLKS', 'POLLS', 'POLLY', 'POLOS', 'POLTS', 'POLYS', 'POMBE', 'POMES',
     'POMMY', 'POMOS', 'POMPS', 'PONCE', 'PONCY', 'PONDS', 'PONES', 'PONEY', 'PONGA', 'PONGO',
     'PONGS', 'PONGY', 'PONKS', 'PONTS', 'PONTY', 'PONZU', 'POOCH', 'POODS', 'POOED', 'POOHS',
@@ -2328,7 +2328,7 @@ export const dailyWordsLarge = [
     'PYXIE', 'PYXIS', 'PZAZZ',
     'QADIS', 'QAIDS', 'QAJAQ', 'QANAT', 'QAPIK', 'QIBLA', 'QOPHS', 'QORMA', 'QUACK', 'QUADS',
     'QUAFF', 'QUAGS', 'QUAIL', 'QUAIR', 'QUAIS', 'QUAKE', 'QUAKY', 'QUALE', 'QUANT',
-    'QUARE', 'QUARK', 'QUASH', 'QUASI', 'QUASS', 'QUATE', 'QUATS', 'QUAYD', 'QUAYS',
+    'QUARE', 'QUARK', 'QUASI', 'QUASS', 'QUATE', 'QUATS', 'QUAYD', 'QUAYS',
     'QUBIT', 'QUEAN', 'QUEER', 'QUELL', 'QUEME', 'QUENA', 'QUERN',
     'QUEYN', 'QUEYS', 'QUICH', 'QUIDS', 'QUIFF', 'QUILL', 'QUILT',
     'QUIMS', 'QUINA', 'QUINE', 'QUINO', 'QUINS', 'QUINT', 'QUIPO', 'QUIPS', 'QUIPU', 'QUIRE',
@@ -11395,7 +11395,19 @@ export const wordleWords = [
         gameDate: "5/27/25",
         myScore: 2,
         wordNumber: 1438,
-      }
+      },
+      {
+        word: "POLAR",
+        gameDate: "5/28/25",
+        myScore: 2,
+        wordNumber: 1439,
+      },
+      {
+        word: "QUASH",
+        gameDate: "5/29/25",
+        myScore: 4,
+        wordNumber: 1440,
+      },
   ];
 
 export const letterValues = {
