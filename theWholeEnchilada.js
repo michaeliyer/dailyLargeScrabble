0932ae751855c2529447c30f321cbd7a6984fffe
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-01-19 23:43:19 -0500

    Waxen Dealt With, For Real

--- theWholeEnchilada.js
+++ theWholeEnchilada.js
@@ -12668,7 +12668,7 @@ export const wordleWords = [
 
     
   ];
-
+ 
 export const letterValues = {
     A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1,
     J: 8, K: 5, L: 1, M: 3, N: 1, O: 1, P: 3, Q: 10, R: 1,
